d4119f5c948a141e058242a058218f15fde2483e
Satoshi Moriai <moriai.satoshi@ntt-tx.co.jp>
2021-05-02 12:40:55 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# tinyhttpd
+
+**tinyhttpd** is an example of crate tiny_httpd, 
+
+The original source code can be found [here](https://github.com/tiny-http/tiny-http).
+
+## License
+
+The original project is licensed under either of
+
++ Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
++ MIT license ([LICENSE-MIT](LICENSE-MIT) or
+  http://opensource.org/licenses/MIT)
+
+at your option.
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+// tinyhttpd - An example of crate tiny_http
+//  derived from https://github.com/tiny-http/tiny-http/blob/0.8.1/examples/serve-root.rs
+
+use ascii::AsciiString;
+use std::fs;
+use std::path::Path;
+use std::env;
+use log::info;
+use simplelog::{ConfigBuilder, SimpleLogger, LevelFilter};
+
+fn get_content_type(path: &Path) -> &'static str {
+    let extension = match path.extension() {
+        None => return "text/plain",
+        Some(e) => e,
+    };
+
+    match extension.to_str().unwrap() {
+        "gif" => "image/gif",
+        "jpg" => "image/jpeg",
+        "jpeg" => "image/jpeg",
+        "png" => "image/png",
+        "pdf" => "application/pdf",
+        "js" => "application/javascript; charset=utf8",
+        "htm" => "text/html; charset=utf8",
+        "html" => "text/html; charset=utf8",
+        "txt" => "text/plain; charset=utf8",
+        _ => "text/plain; charset=utf8",
+    }
+}
+
+fn main() {
+    let config = ConfigBuilder::new()
+        .set_time_to_local(true)
+        .set_time_format("%b %e %T".to_string())
+        .build();
+    SimpleLogger::init(LevelFilter::Info, config).unwrap();
+
+    let curdir = env::current_dir().unwrap();
+    info!("The current directory is {}", curdir.display());
+
+    let server = tiny_http::Server::http("0.0.0.0:8000").unwrap();
+    let port = server.server_addr().port();
+    info!("Now listening on port {}", port);
+
+    loop {
+        let rq = match server.recv() {
+            Ok(rq) => rq,
+            Err(_) => break,
+        };
+
+        info!("{:?}", rq);
+
+        let url = rq.url().to_string();
+        let path = Path::new(&url).strip_prefix("/").unwrap();
+        let file = fs::File::open(&path);
+
+        if file.is_ok() {
+            let response = tiny_http::Response::from_file(file.unwrap());
+
+            let response = response.with_header(tiny_http::Header {
+                field: "Content-Type".parse().unwrap(),
+                value: AsciiString::from_ascii(get_content_type(&path)).unwrap(),
+            });
+
+            let _ = rq.respond(response);
+        } else {
+            let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(404));
+            let _ = rq.respond(rep);
+        }
+    }
+}
